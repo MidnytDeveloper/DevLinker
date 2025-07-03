fd39933defe0dcd44b3264f8a4bba37e1f61a3e6
MidnytDeveloper <abhishek.sni101@gmail.com>
2025-07-03 23:13:25 +0530

    auth middlwares

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
-app.use((req, res) => {
-  res.send("Hello from the server");
+
+const { adminAuth } = require("./middlewares/auth");
+
+app.use("/admin", adminAuth);
+
+app.get("/user", (req, res) => {
+  res.send("user data");
 });
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All Data sent");
+});
+
+app.get("/admin/DeleteUser", (req, res) => {
+  res.send("user Deleted");
+});
+
 app.listen(3000, () => {
   console.log("server is created successfully and listen on port 3000");
 });
